df625389dc6a4038e1331a101699ae8c5745dda4
juanpabloletteri <pabloletteri@gmail.com>
2018-05-08 14:44:25 -0300

    routeo y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,27 @@ import { PrimengModule } from './modulos/primeng/primeng.module';
 import { RouterModule, Route, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TablaComponent } from './componentes/tabla/tabla.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 const config: Routes = [
   {
     path: '',
-    component: AppComponent
+    component: HomeComponent
+  },
+  {
+    path:'tabla',
+    component: TablaComponent
   }
 ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
