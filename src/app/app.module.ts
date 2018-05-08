8e41aa8886437f3130757fd485e3765f623e1203
juanpabloletteri <pabloletteri@gmail.com>
2018-05-08 14:25:18 -0300

    inicial con prime ng y routeo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { PrimengModule } from './modulos/primeng/primeng.module';
+
+import { RouterModule, Route, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+const config: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    PrimengModule,
+    RouterModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
