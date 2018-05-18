1e509bd5af0093c64f486e70744aa09e9d76e38a
juanpabloletteri <pabloletteri@gmail.com>
2018-05-17 23:11:27 -0300

    correccion clase faltante

--- src/app/servicios/nexo.service.ts
+++ src/app/servicios/nexo.service.ts
@@ -1,40 +1,9 @@
 import { Injectable } from '@angular/core';
-import { item } from '../clases/item';
+
 
 @Injectable()
 export class NexoService {
 
-  constructor(private miItem:item) { }
+  constructor() { }
 
-  setId(id) {
-    this.miItem.id = id;
-  }
-  setNombre(nombre) {
-    this.miItem.nombre = nombre;
-  }
-  setCantidad(cantidad) {
-    this.miItem.cantidad = cantidad;
-  }
-  setPrecio(precio) {
-    this.miItem.precio = precio;
-  }
-  setDescipcion(descripcion) {
-    this.miItem.descripcion = descripcion;
-  }
-  /////////////////////////
-  getId() {
-    return this.miItem.id;
-  }
-  getNombre() {
-    return this.miItem.nombre;
-  }
-  getCantidad() {
-    return this.miItem.cantidad;
-  }
-  getPrecio() {
-    return this.miItem.precio;
-  }
-  getDescripcion() {
-    return this.miItem.descripcion;
-  }
 }
